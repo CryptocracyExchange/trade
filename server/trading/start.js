578c61d402ad5b6fdbfd76c4099badbee164d521
Kai Hsia <kaihsia@gmail.com>
2017-01-03 23:57:42 -0800

    Fulfill partial orders

--- server/trading/start.js
+++ server/trading/start.js
@@ -14,6 +14,31 @@ let openSell = connect.record.getList('openSell');
 // Sell Transaction History
 let histSell = connect.record.getList('histSell');
 
+
+/** Create test open sell list **/
+// openSell.whenReady((newList) => {
+//   for (let h = 0; h < 5; h++) {
+//     let unique = connect.getUid();
+//     let newSellRecord = connect.record.getRecord(`transaction/sell/${unique}`);
+//     newSellRecord.whenReady((newRec) => {
+//       newRec.set({
+//         sell: {
+//           amount: Math.ceil(Math.random()*10),
+//           price: Math.ceil(Math.random()*100)
+//         }
+//       }, err => {
+//         if (err) {
+//           console.log('Sell record set with error:', err)
+//         } else {
+//           console.log('Sell record set without error:');
+//           newList.addEntry(`transaction/sell/${unique}`);
+//         }
+//       });
+//     });
+//   }
+// });
+
+
 // Define the buy method
 module.exports.buy = () => {
   connect.event.subscribe('transaction/buy', (data) => {
@@ -57,25 +82,55 @@ module.exports.buy = () => {
             });
 
             let entres = list.getEntries();
-            console.log('list list', entres);
             for (let i = 0; i < entres.length; i++) {
               connect.record.getRecord(entres[i]).whenReady((reco) => {
                 let pr = reco.get('buy');
-                console.log('new list: ', pr, reco.name);
               });
             }
           });
-        }
-      });
-      // Check sell orders to fulfill open buy order
-      openSell.whenReady((sellList) => {
-        let sellOrders = sellList.getEntries();
-        // console.log(sellOrders);
-        for (let n = 0; n < sellOrders.length; n++) {
-          connect.record.getRecord(sellOrders[i]).whenReady((recon) => {
-            let sellRecord = recon.get('sell');
-            if (sellRecord.price <= data.price) {
-              
+
+          // Check sell orders to fulfill open buy order
+          openSell.whenReady((sellList) => {
+            let sellOrders = sellList.getEntries();
+            let diff;
+            for (let n = 0; n < sellOrders.length; n++) {
+              connect.record.getRecord(sellOrders[n]).whenReady((recon) => {
+                let sellRecord = recon.get('sell');
+                histBuy.whenReady((transList) => {
+                  openBuy.whenReady((buyList) => {
+                    histSell.whenReady((histSellList) => {
+                      connect.record.getRecord(`transaction/buy/${unique}`).whenReady((thisRecord) => {
+                        let amount = thisRecord.get('buy.amount');
+                        let pricing = thisRecord.get('buy.price');
+                        console.log('amount', amount);
+                        if (sellRecord && sellRecord.amount && sellRecord.price <= pricing) {
+                          if (sellRecord.amount == amount) {
+                            transList.addEntry(thisRecord.name);
+                            histSellList.addEntry(recon.name);
+                            sellList.removeEntry(recon.name);
+                            buyList.removeEntry(thisRecord.name);
+                          } else if (sellRecord.amount < amount) {
+                            diff = amount - sellRecord.amount;
+                            if (diff > 0) {
+                              histSellList.addEntry(recon.name);
+                              sellList.removeEntry(recon.name);
+                              thisRecord.set('buy.amount', diff);
+                            }
+                          } else {
+                            // sellRecord.amount > amount
+                            if (diff == 0) {
+                              translist.addEntry(thisRecord.name);
+                              buyList.removeEntry(thisRecord.name);
+                            } else {
+                              recon.set('sell.amount', diff);
+                            }
+                          }
+                        }
+                      });
+                    });
+                  });
+                });
+              });
             }
           });
         }
@@ -140,9 +195,12 @@ module.exports.sell = () => {
   });
 };
 
-// Test
+
+
+/** Test **/
+// Open Buy Orders
 openBuy.whenReady((list) => {
-  console.log('buy', list.getEntries());
+  // console.log('buy', list.getEntries());
   var entries = list.getEntries();
 
   for (var i = 0; i < entries.length; i++) {
@@ -155,19 +213,48 @@ openBuy.whenReady((list) => {
   }
 });
 
+// Open Sell Orders
 openSell.whenReady((list) => {
-  console.log('sell', list.getEntries());
+  // console.log('sell', list.getEntries());
   const entries = list.getEntries();
-  var prices = [];
+  // var prices = [];
   for (var i = 0; i < entries.length; i++) {
-
     connect.record.getRecord(entries[i]).whenReady((record) => {
       // console.log('FJLEJLKJF', record);
       let price = record.get('sell');
-      console.log('sell list: ', price);
+      console.log('sell list: ', price, record.name);
       // console.log('sell price: ', record.get('price'));
     });
   }
   // console.log('sell array: ', prices);
+});
 
+// Transaction History Buy Orders
+histBuy.whenReady((list) => {
+  // console.log('histBuy', list.getEntries());
+  var entries = list.getEntries();
+
+  for (var i = 0; i < entries.length; i++) {
+    connect.record.getRecord(entries[i]).whenReady((record) => {
+      // console.log('FJLEJLKJF', record);
+      let price = record.get('buy');
+      console.log('buy history: ', price, record.name);
+      // record.set('buy.amount', '423');
+    });
+  }
+});
+
+// Transaction History Sell Orders
+histSell.whenReady((list) => {
+  // console.log('histSell', list.getEntries());
+  var entries = list.getEntries();
+
+  for (var i = 0; i < entries.length; i++) {
+    connect.record.getRecord(entries[i]).whenReady((record) => {
+      // console.log('FJLEJLKJF', record);
+      let price = record.get('sell');
+      console.log('sell history: ', price, record.name);
+      // record.set('buy.amount', '423');
+    });
+  }
 });
